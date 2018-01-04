09c94d31f345623b842acdb791346b4eaea3baf9
Mark Avery <mark@webark.cc>
2018-01-04 08:49:02 -0800

    [BUGFIX] removing depreciated nodeModulesPath.
    based off of https://github.com/ember-cli/ember-cli/pull/7401 `nodeModulesPath` has been depreciated, and `resolveSync` is a preferred alternative. `nodeModulesPath` has been removed in Ember Cli 3.x and it's use here breaks 3.x builds.

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 /* jshint node: true */
 'use strict';
 
-var path = require('path');
 var Funnel = require('broccoli-funnel');
 
 module.exports = {
   name: 'ember-normalize',
 
   treeForStyles: function() {
-    var normalizePath = path.join(this.project.nodeModulesPath, 'normalize.css');
+    var normalizePath = this.project.resolveSync('normalize.css');
     var normalizeTree = new Funnel(this.treeGenerator(normalizePath), {
       srcDir: '/',
       destDir: '/app/styles'
