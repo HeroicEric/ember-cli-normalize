5d15d49f03f9441f7c748f262e26745a1889c27b
Mark Avery <mark@webark.cc>
2018-01-04 11:30:46 -0800

    Using the funnel to bring in just the file.

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
 var Funnel = require('broccoli-funnel');
 
 module.exports = {
   name: 'ember-normalize',
 
   treeForStyles: function() {
-    var normalizePath = this.project.resolveSync('normalize.css');
-    var normalizeTree = new Funnel(this.treeGenerator(normalizePath), {
-      srcDir: '/',
-      destDir: '/app/styles'
+    var normalizeModulePath = path.dirname(this.project.resolveSync('normalize.css'));
+    var normalizeTree = new Funnel(this.treeGenerator(normalizeModulePath), {
+      destDir: 'app/styles',
+      include: ['normalize.css']
     });
 
     return normalizeTree;
